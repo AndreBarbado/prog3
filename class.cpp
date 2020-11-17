8b5a0c66c6f4ae72cef587e2541a91318b35cfbe
AndreBarbado <54245771+AndreBarbado@users.noreply.github.com>
2020-11-17 17:42:59 -0300

    Create class.cpp

---
+++ class.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <string.h>
+
+class parque{
+    public:
+
+    private:
+    protected:
+}
+
+class parque{
+    public:
+    
+    private:
+    protected:
+}
+
+class parque{
+    public:
+    
+    private:
+    protected:
+}
+
+class parque{
+    public:
+    
+    private:
+    protected:
+}
+
+class produtos{
+    public:
+    
+    private:
+    protected:
+}
+
+class lote{
+    public:
+    
+    private:
+    protected:
+}
+
+class vendas{
+    public:
+    
+    private:
+    protected:
+}
+
+class consiguinado{
+    public:
+    
+    private:
+    protected:
+}
+
+class cliente{
+    public:
+    
+    private:
+    protected:
+}
+
+class forma_pagamento{
+    public:
+    
+    private:
+    protected:
+}
+
+class maquina_de_cartao{
+    public:
+    
+    private:
+    protected:
+}
+
+class bandeira{
+    public:
+    
+    private:
+    protected:
+}
+
+
