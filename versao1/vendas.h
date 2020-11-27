3b603f881b3dfadc7bca219734b21dbf8c9f19ae
Malu Fontes <63003278+malufontes@users.noreply.github.com>
2020-11-27 12:39:36 -0300

    Update vendas.h

--- versao1/vendas.h
+++ versao1/vendas.h
@@ -2,63 +2,98 @@
 #define VENDAS_H
 
 #include <iostream>
+#include <array>
 
 #include "data.h"
 
+class Empresa{
+    std::array<float,12> receita_mensal;
 
-enum class Forma_de_Pagamento {Cartao_credito, Cartao_debito, Dinheiro};
+public:
+    friend class Venda;
+    friend class Dinheiro;
+    friend class Cartao_Debito;
+    friend class Cartao_Credito;
+
+    Empresa();
+
+    void print(){
+        for (const auto& it : receita_mensal){
+            std::cout << it << " ";
+        }
+        std::cout << std::endl;
+    }
+};
+
+
+enum class Forma_de_Pagamento {CARTAO_CREDITO, CARTAO_DEBITO, DINHEIRO};
 
 class Venda{
     friend std::ostream &operator<< (std::ostream &, const Venda & );
 
+    Empresa empresa_creditada;
     Forma_de_Pagamento formapagamento;
-    void print_forma_de_pagamento();
     Data data_de_venda;
     float valor;
     unsigned int parcelas = 1;
 
-    friend class Pagamento;
-
     //Tratamento de estoque
 
 public:
-    Venda(Forma_de_Pagamento formapagamento, Data data_de_venda, float valor, unsigned int parcelas = 1);
+    Venda(Empresa empresa_creditada, Forma_de_Pagamento formapagamento, Data data_de_venda, float valor, unsigned int parcelas = 1);
 
-    Venda(){}
+    Venda() {};
+
+    virtual ~Venda() {};
+
+    virtual void pagar();
         
 };
 
 
-class Pagamento{
+class Dinheiro: public Venda{
+public:
+    void pagar(Empresa& empresa_creditada, Data data_de_pfmt, float valor);
+
+};
+
+
+enum class Cartao {VISAMASTER, ELO, HIPERCARD};
+
+// fazer vetor com os valores das taxas, os vetores leem as planilhas
+
+class Taxas_bandeiras{
 protected:
-    Venda v;
+    std::array<float,12> c_visaemaster;
+    std::array<float,12> c_elo;
+    std::array<float,12> c_hiper;
+    float d_visaemaster;
+    float d_elo;
+    float d_hiper;
+
+private:
+    void ler_taxas();
 
 public:
-    Pagamento(){};
-    Pagamento(Venda v);
+    Taxas_bandeiras();
+};
 
-    virtual void pagar();
+class Cartao_Debito: public Venda, public Taxas_bandeiras{
+    Cartao bandeira_do_cartao;
+public:
+    Cartao_Debito();
+    
+    void pagar(Empresa empresa_creditada, Data data_de_pfmt, float valor);
 
 };
 
-
-class Dinheiro: public Pagamento{
+class Cartao_Credito: public Venda, public Taxas_bandeiras{
+    Cartao bandeira_do_cartao;
 public:
-    Dinheiro(){};
+    Cartao_Credito();
 
-    void pagar(Data data_de_pfmt, float valor);
-
+    void pagar(Empresa empresa_creditada, Data data_de_pfmt, float valor, unsigned int parcelas);
 };
 
-class Cartao_Debito: public Pagamento{
-public:
-    void pagar();
-
-};
-
-class Cartao_Credito: public Pagamento{
-public:
-    void pagar();
-};
 
 #endif
